43a5e22906a16373b32ddbd921388014be9aea48
Fernando-Gabreil-Tesori <ftesorie123@gmail.com>
2025-05-12 09:43:07 -0300

    Atualização

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,12 @@
 import { FastifyInstance } from "fastify";
 
 async function  routes(fastify: FastifyInstance) {
-    
+     fastify.get('/health', async()=>{
+        return{
+            status: 'ok',
+            message: 'DevBills API rodando normalmente'
+        }
+     })
 }
+
+export default routes
