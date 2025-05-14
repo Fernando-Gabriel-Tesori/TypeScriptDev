3faf4a6db40c2cf3a8c6861b53712275dd94f320
Fernando-Gabreil-Tesori <ftesorie123@gmail.com>
2025-05-14 11:54:24 -0300

    att

--- src/controllers/transactions/createTransaction.controller.ts
+++ src/controllers/transactions/createTransaction.controller.ts
@@ -1,5 +1,11 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
 
-const createTransaction = async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {};
+const createTransaction = async (request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+  const userId = "FED$%DF%RDF";
+
+  if (!userId) {
+    reply.status(401).send({ error: "Usuário não autenticado" });
+  }
+};
 
 export default createTransaction;
