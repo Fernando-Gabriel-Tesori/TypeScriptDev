d46647152d2bfc09109b6294b0488e53776d7821
Fernando-Gabreil-Tesori <ftesorie123@gmail.com>
2025-05-18 16:51:07 -0300

    modified:   src/config/env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,14 +7,14 @@ const envSchema = z.object({
   PORT: z.string().transform(Number).default("3001"),
   DATABASE_URL: z.string().min(8, "DATABASE_URL it has to be mandatory!"),
   NODE_ENV: z.enum(["dev", "test", "prod"], {
-    message: "NODE_ENV it must be dev, test or prod!",
+    message: "NODE_ENV must be dev, test or prod!",
   }),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
-  console.error("🚨 Invalid environment variables!");
+  console.error("🚨 Invalid environment variables!", _env.error.format());
   process.exit(1);
 }
 
